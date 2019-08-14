cb764264427bdeacabc0d4faefda965db708255c
Lau <laststandrighthere@gmail.com>
2019-08-15 05:59:20 +0800

    flasher: Import AK2
    Signed-off-by: Lau <laststandrighthere@gmail.com>

---
+++ anykernel.sh
@@ -0,0 +1,35 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=vince
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. $TMPDIR/tools/ak2-core.sh;
+
+## AnyKernel install
+ui_print "  • Unpacking image"
+dump_boot;
+
+# begin ramdisk changes
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
+
